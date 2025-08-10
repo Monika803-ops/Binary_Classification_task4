49b54d9f9f95f482fef44a802b8aa58bdd797d0f
Monika803-ops <monikanv175@gmail.com>
2025-08-10 15:41:27 +0530

    Update task4.py

--- task4.py
+++ task4.py
@@ -7,15 +7,13 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import confusion_matrix, classification_report, roc_auc_score, roc_curve
 
-from sklearn.datasets import load_breast_cancer
-data = load_breast_cancer()
-df = pd.DataFrame(data.data, columns=data.feature_names)
-df['target'] = data.target
+
+df = pd.read_csv('data.csv')  
 print("Dataset Shape:", df.shape)
 print(df.head())
-
-X = df.drop('target', axis=1)
+X = df.drop('target', axis=1)  
 y = df['target']
+
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42
 )
@@ -27,12 +25,15 @@ X_test = scaler.transform(X_test)
 model = LogisticRegression()
 model.fit(X_train, y_train)
 
+
 y_pred = model.predict(X_test)
 y_pred_prob = model.predict_proba(X_test)[:, 1]
 
 cm = confusion_matrix(y_test, y_pred)
 plt.figure(figsize=(5,4))
-sns.heatmap(cm, annot=True, fmt="d", cmap="Blues", xticklabels=["Malignant", "Benign"], yticklabels=["Malignant", "Benign"])
+sns.heatmap(cm, annot=True, fmt="d", cmap="Blues",
+            xticklabels=["Malignant", "Benign"],
+            yticklabels=["Malignant", "Benign"])
 plt.xlabel("Predicted")
 plt.ylabel("Actual")
 plt.title("Confusion Matrix")
